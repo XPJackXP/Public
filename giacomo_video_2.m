4e76f05490abc3b09054cd7a2aff623f9364778b
Giacomo <39993024+XPJackXP@users.noreply.github.com>
2020-07-10 12:43:08 +0200

    Update giacomo_video_2.m
    Octace_1_12

--- giacomo_video_2.m
+++ giacomo_video_2.m
@@ -2,49 +2,7 @@
 # Materia 6:  Lezione 2       15/01/2020
 # Immagini    Gabriele Simone
 
-####################
 
-# prompt Octave
-
-# Parte_Sportarsi_1
-# exit directory
-# / \ local
-cd /   #  / Directoru
-cd #   #  # Io
-cd d:/
-cd c
-
-# help
-help
-info 
-help cd
-info /
-/ -?
-cd -h
-
-# enter Disk dir
-dir
-ls
-ls -l # ll elle (l)
-cd /dev
-cd \dev
-cd usr/var
-# enter Volume
-cd 007\ -\ Simone\ -\ Video/ # SI
-cd "007 - Simone - Video"/   # SI
-cd "007 - Simone - Video"    # NO
-# cd "007\ -\ Simone\ -\ Video" # no
-cd "/d/jack_/Utenti/003 - Desktop/005 - Istruzione/004 - corsi secondari/IFTS - Luigi Clerici 2020/007 - Simone - Video/Gabriele Simone ZIP/SimoneZip_1/Revisione_Lezione_2/Simone_workspace_1"/
-
-# \ enter  = Move to dir 
-# / exec   = Execution file
-
-# Parte_configFile_2
-start delciampo_video_1.m
-
-# Creare e Salvare File
-
-####################
 # pkg install package
 # install image
 # install control
